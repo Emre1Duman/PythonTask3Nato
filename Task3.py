eed6be204fbb98fb3543e635a93e5fa0ade325ce
unknown <emred2010@hotmail.com>
2022-04-20 17:06:48 +0100

    Task 3 Nato alphabet

---
+++ Task3.py
@@ -0,0 +1,28 @@
+#Message = ('If, you can read?')
+#UserInput = input('Enter Message: ')
+
+
+d =  {
+        'A': 'Alpha',  'B': 'Bravo',   'C': 'Charlie',
+        'D': 'Delta',  'E': 'Echo',    'F': 'Foxtrot',
+        'G': 'Golf',   'H': 'Hotel',   'I': 'India',
+        'J': 'Juliett','K': 'Kilo',    'L': 'Lima',
+        'M': 'Mike',   'N': 'November','O': 'Oscar',
+        'P': 'Papa',   'Q': 'Quebec',  'R': 'Romeo',
+        'S': 'Sierra', 'T': 'Tango',   'U': 'Uniform',
+        'V': 'Victor', 'W': 'Whiskey', 'X': 'X-ray',
+        'Y': 'Yankee', 'Z': 'Zulu'}
+
+    
+UserInput = input('Enter Message: ')
+for letter in UserInput:
+        print(d.get(letter.upper()), end=' ')
+
+#print(' '.join(d.get(letter.upper(), letter) for letter in UserInput))
+
+#file = open("Task3.txt", "r")
+
+#print("First line: ")
+ 
+
+  #  file.close()
